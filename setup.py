78189ce0ea8f40fea80414c9bbf6b70ddea34f41
ajaharleo <ajaharuddin10217@gmail.com>
2022-07-02 10:12:27 +0530

    component and config parts done

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements_list()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         return requirement_file.readlines().remove("-e .")
 
-
+setup()
 setup(
 name=PROJECT_NAME,
 version=VERSION,
